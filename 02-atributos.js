c0cdcadb766b585ceaea52369f1fce73f8f046c2
marcely <arrudamarcely460@gmail.com>
2024-09-12 08:40:54 -0400

    add atribute POO

---
+++ 02-atributos.js
@@ -0,0 +1,9 @@
+class Pessoa {
+    constructor(nome, idade, altura, peso) { // Atributos
+        // Inicialização dos atributos
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
