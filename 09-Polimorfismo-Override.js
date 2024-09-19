033e868d76a5b9e44ef04b382b15963a65b7b6df
marcely <arrudamarcely460@gmail.com>
2024-09-19 10:48:18 -0400

    add classe arqueiro

--- 09-Polimorfismo-Override.js
+++ 09-Polimorfismo-Override.js
@@ -36,3 +36,15 @@ class Mago extends Personagem {
         console.log(`${this.nome} lança ${this.feitico}!`);
     }
 }
+
+//Classe Arqueiro que herda de Personagem 
+    class Arqueiro extends Personagem {
+        constructor(nome, vida, tipoFlecha){
+            super(nome, vida);
+            this.tipoFlecha = tipoFlecha;
+    }
+
+    atacar(){
+        console.log(`${this.nome} dispara flecha ${this.tipoFlecha}!`);
+    }
+}
