787636fd0d68bfb3fb7c9e3b993490f1aa94cc81
marcely <arrudamarcely460@gmail.com>
2024-09-19 07:42:07 -0400

    add exemplo heranca

---
+++ 07-heranca.js
@@ -0,0 +1,9 @@
+//Classe base Pessoa
+class Pessoa {
+    constructor(nome, idade, altura, peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
