a320b5756eb3ac94dfab0714cd83b5e6c2b08224
marcely <arrudamarcely460@gmail.com>
2024-09-19 10:23:22 -0400

    add polimorfismo override

---
+++ 09-Polimorfismo-Override.js
@@ -0,0 +1,16 @@
+//Classe base Personagem
+class Personagem {
+    constructor(nome, vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    atacar() {
+        console.log(`${this.nome} realiza um ataque básico!`);
+    }
+
+    receberDano(dano) {
+        this.vida -= dano;
+        console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}`);
+    }
+}
