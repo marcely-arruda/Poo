67c1357b178536b86f9a6197f3be90723b497391
marcely <arrudamarcely460@gmail.com>
2024-09-19 08:13:33 -0400

    add exemplo de funcionario

--- 07-heranca.js
+++ 07-heranca.js
@@ -30,3 +30,10 @@ class Diretor extends Funcionario {
         super(nome, idade, altura, peso, salario);
     }
 }
+
+//Class Professor que herda de Funcionario
+class Professor extends Funcionario {
+    constructor(nome, idade, altura, peso, salario) {
+        super(nome, idade, altura, peso, salario);
+    }
+}
