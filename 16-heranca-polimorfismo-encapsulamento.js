631325bc589bb7b10a032786cc76af605b606dee
marcely <arrudamarcely460@gmail.com>
2024-10-01 08:20:19 -0400

    heranca polimorfismo encapsulamento

---
+++ 16-heranca-polimorfismo-encapsulamento.js
@@ -0,0 +1,53 @@
+//Classe base Personagem
+class Personagem {
+  //Encapsulando os atributos com #, tornando-os privados
+  #nome;
+  #classe;
+  #nivel;
+  #vida;
+  #mana;
+  //Método Construtor
+  constructor(nome, classe, nivel, vida, mana) {
+    this.#nome = nome;
+    this.#classe = classe;
+    this.#nivel = nivel;
+    this.#vida = vida;
+    this.#mana = mana;
+  }
+  //Métodos Getters e Setters
+  //Métodos Getter e Setter para o atributo nome
+  get nome() {
+    return this.#nome;
+  }
+  set nome(nome) {
+    this.#nome = nome;
+  }
+  //Métodos Getter e Setter para o atributo classe
+  get classe() {
+    return this.#classe;
+  }
+  set classe(classe) {
+    this.#classe = classe;
+  }
+  //Métodos Getter e Setter para o atributo nivel
+  get nivel() {
+    return this.#nivel;
+  }
+  set nivel(nivel) {
+    this.#nivel = nivel;
+  }
+  //Métodos Getter e Setter para o atributo vida
+  get vida() {
+    return this.#vida;
+  }
+  set vida(vida) {
+    this.#vida = vida;
+  }
+  //Métodos Getter e Setter para o atributo mana
+  get mana() {
+    return this.#mana;
+  }
+  set mana(mana) {
+    this.#mana = mana;
+  }
+}
